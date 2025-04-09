015efd7302639487f3d738dcf70150a0ab210940
Kamiokandeed <giovanni.useche@hotmail.com>
2025-04-08 23:29:15 -0400

    I added the Footer component as a shared component among all the other components, and I also added the routes for the new menu options that were missing, such as news, events, and bursaries.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
+import { MembershipComponent } from './membership/membership.component';
+import { EventsComponent } from './events/events.component';
+import { NewsComponent } from './news/news.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { BursariesComponent } from './bursaries/bursaries.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -10,7 +14,11 @@ export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'education', component: EducationComponent },
+    { path: 'membership', component: MembershipComponent },
+    { path: 'events', component: EventsComponent },
+    { path: 'news', component: NewsComponent },
     { path: 'resources', component: ResourcesComponent },
+    { path: 'bursaries', component: BursariesComponent },
     { path: 'gallery', component: GalleryComponent },
     { path: 'contact', component: ContactComponent },
 ];
