553bead6c9a34c726f7d92d8f216da37222e655f
Kamiokandeed <giovanni.useche@hotmail.com>
2025-04-08 23:05:06 -0400

    Added the missing new menu components

---
+++ src/app/bursaries/bursaries.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bursaries',
+  imports: [],
+  templateUrl: './bursaries.component.html',
+  styleUrl: './bursaries.component.css'
+})
+export class BursariesComponent {
+
+}
