7bef87c8bf515d12698a9535dbf23d766fdbb813
Brad Buttineau <113149072+bradbuttineau@users.noreply.github.com>
2025-04-13 17:59:41 -0400

    validation of forms in frontend for sign-in and sign up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   imports: [],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
+// sign-up validation
 export class SignupComponent {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
+  ngOnInuit(): void {
+    this.signUpForm = this.fb.group({
+
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      postalCode: ['', Validators.required],
+
+
+    })
+
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+  onSignUp() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value)
+    } else {
+      this.validateAllFormFields(this.signUpForm)
+      // logic for throwing error
+    }
+  }
+private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormGroup) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control)
+      }
+    })
+  }
 }
